5478daffb67fe4ad4c58cfa7f10271a8f947919b
Stanislau Kulibaba <stanislav.kulibaba@currency.com>
2019-09-10 12:58:24 +0300

    fux(export) changed the way to export initialize func

--- www/SearchAds.js
+++ www/SearchAds.js
@@ -1,21 +1,14 @@
-var exec = cordova.require('cordova/exec');
+var exec = require('cordova/exec');
 
-var SearchAds = function() {
-    console.log('SearchAds instanced');
-};
+function initialize(onSuccess, onError) {
+    // to use in Promise way
+  if (!onSuccess) {
+      return new Promise(function (resolve, reject) {
+          initialize(resolve, reject);
+      });
+  }
 
-SearchAds.prototype.initialize = function(onSuccess, onError) {
-    var errorCallback = function(obj) {
-        onError(obj);
-    };
-
-    var successCallback = function(obj) {
-        onSuccess(obj);
-    };
-
-    exec(successCallback, errorCallback, 'SearchAds', 'initialize');
-};
-
-if (typeof module != 'undefined' && module.exports) {
-    module.exports = SearchAds;
+    exec(onSuccess, onError, 'SearchAds', 'initialize');
 }
+
+exports.initialize = initialize;
